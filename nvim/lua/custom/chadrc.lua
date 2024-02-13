acbf447819ee19af6ed414114cf58a60e9e8163c
zhenya-paitash <naitavv93@gmail.com>
2024-02-13 18:59:14 +0300

    nvim 13.02.2024
    ADD: codeium AI autocomplete plugin
    UPD: plugins

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyodark",
-  theme_toggle = { "tokyodark", "ayu_light" },
+  theme = "pastelbeans",
+  theme_toggle = { "pastelbeans", "ayu_light" },
   transparency = false,
   lsp_semantic_tokens = true,
 
--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -256,6 +256,29 @@ local plugins = {
   --   ft = { "go", "gomod" },
   --   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   -- },
+
+  -- AI --
+  -- autocomplete
+  {
+    "Exafunction/codeium.vim",
+    -- lazy = false,
+    event = 'BufEnter',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
+  },
 }
 
 return plugins
