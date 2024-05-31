af831af35e75d254a90773306374a7391dd58e44
zhenya-paitash <zhenyapaitash@gmail.com>
2024-05-31 14:47:11 +0300

    nvim plugin: add `folke/noice.nvim` -- for UI plugin: add `m4xshen/hardtime.nvim`  -- for use best practice in my workflow

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -210,4 +210,25 @@ return {
     config = require("plugins.setup.obsidian").config,
     -- dependencies = { "nvim-lua/plenary.nvim" },
   },
+
+  { -- UI
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = require("plugins.setup.ui").config,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
+    },
+  },
+
+  { -- BEST PRACTICE
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    -- cmd = { "Hardtime" },
+    opts = {},
+  },
 }
