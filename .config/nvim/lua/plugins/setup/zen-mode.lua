01102242dd9a65d56db99902a28cde5975a1f805
zhenya-paitash <zhenyapaitash@gmail.com>
2024-08-09 20:55:22 +0300

    update: daily system update

--- .config/nvim/lua/plugins/setup/zen-mode.lua
+++ .config/nvim/lua/plugins/setup/zen-mode.lua
@@ -74,7 +74,7 @@ return {
         term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
         inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
       },
-      context = 10, -- amount of lines we will try to show around the current line
+      context = 20, -- amount of lines we will try to show around the current line
       treesitter = true, -- use treesitter when available for the filetype
       -- treesitter is used to automatically expand the visible text,
       -- but you can further control the types of nodes that should always be fully expanded
