2e18b446ecd4868a9c21aa8ae899a95c6851c49c
zhenya-paitash <zhenyapaitash@gmail.com>
2025-03-24 13:30:04 +0300

    update: daily system update

--- .config/ags/src/widgets/Notifications/notifications.ts
+++ .config/ags/src/widgets/Notifications/notifications.ts
@@ -31,10 +31,10 @@ export function Notifications(monitor: number = 0) {
     monitor,
     name: `notifications-${monitor}`,
     className: 'notification-popups',
-    anchor: ['top'],
-    margins: [0, 0, 0, 0],
-    // anchor: ['top', 'right'],
-    // margins: [0, 8, 0, 0],
+    // anchor: ['top'],
+    // margins: [0, 0, 0, 0],
+    anchor: ['top', 'right'],
+    margins: [0, 8, 0, 0],
     layer: 'overlay',
 
     child: Widget.Box({
